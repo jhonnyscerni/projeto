38435ecc98909e27a259159d94a05061f040b9a6
jhonnyscerni <jhonnyscerni@gmail.com>
2021-09-07 14:37:34 -0300

    Ajustes na seguraça

--- src/main/java/br/com/siberius/projeto/infrastructure/service/email/SmtpEnvioEmailService.java
+++ src/main/java/br/com/siberius/projeto/infrastructure/service/email/SmtpEnvioEmailService.java
@@ -10,9 +10,10 @@ import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
-//@Service
+@Service
 public class SmtpEnvioEmailService implements EnvioEmailService {
 
     @Autowired
